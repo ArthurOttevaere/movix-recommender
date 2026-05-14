8ca0821990067f1d5f313a70b5e3c7a31a3fbd13
ArthurOttevaere <arthur.ottevaere@student.uclouvain.be>
2026-05-14 20:46:17 +0200

    Updating Coding 4 with hackathon changes

--- configs.py
+++ configs.py
@@ -10,11 +10,12 @@ class EvalConfig:
         ("baseline_3", ModelBaseline3, {}),
         ("baseline_4", ModelBaseline4, {"random_state": 1}),
         ("KNNwithMeans", ModelBaseline5, {"random_state": 1}),
-        ("UserBased", UserBased, {"k": 3, "min_k": 2, "sim_options": {'name': 'msd', 'user_based': True}, "random_state": 1})
+        #("UserBased", UserBased, {"k": 3, "min_k": 2, "sim_options": {'name': 'msd', 'user_based': True}, "random_state": 1})
+        ("ContentBased", ContentBased, {"features_method": "all_content_tmdb_tags2000", "regressor_method": "ridge_cv"})
     ]
-    split_metrics = ["mae", "rmse"]
-    loo_metrics = ["hit_rate"]
-    full_metrics = ["novelty"]
+    split_metrics = ["rmse"]
+    loo_metrics = []
+    full_metrics = []
 
     # Split parameters
     test_size = 0.25  # -- configure the test_size (from 0 to 1) --
