868fdc90d85a695df79358d05f02f5305171db1e
Skalipp <mohamed.elmohcine@student.uclouvain.be>
2026-05-27 21:10:26 +0200

    Add evaluation metrics and debug sampling

--- configs.py
+++ configs.py
@@ -5,31 +5,31 @@ from models import *
 class EvalConfig:
     
     models = [
-        ("baseline_1", ModelBaseline1, {}),  # model_name, model class, model parameters (dict)
-        ("baseline_2", ModelBaseline2, {}),
-        ("baseline_3", ModelBaseline3, {}),
-        ("baseline_4", ModelBaseline4, {"random_state": 1}),
-        ("KNNwithMeans", ModelBaseline5, {"random_state": 1}),
-        ("UserBased_Manual", UserBased, {"k": 3, "min_k": 2, "sim_options": {'name': 'msd', 'min_support': 3, 'user_based': True}}),
+        # ("baseline_1", ModelBaseline1, {}),  # model_name, model class, model parameters (dict)
+        # ("baseline_2", ModelBaseline2, {}),
+        # ("baseline_3", ModelBaseline3, {}),
+        # ("baseline_4", ModelBaseline4, {"random_state": 1}),
+        # ("KNNwithMeans", ModelBaseline5, {"random_state": 1}),
+        # ("UserBased_Manual", UserBased, {"k": 3, "min_k": 2, "sim_options": {'name': 'msd', 'min_support': 3, 'user_based': True}}),
 
-        ("RandomSample", ContentBased, {"features_method": "title_length", "regressor_method": "random_sample"}),
-        ("RandomScore", ContentBased, {"features_method": "title_length", "regressor_method": "random_score"}),
+        # ("RandomSample", ContentBased, {"features_method": "title_length", "regressor_method": "random_sample"}),
+        # ("RandomScore", ContentBased, {"features_method": "title_length", "regressor_method": "random_score"}),
 
-        ("LinearRegression_Intercept_False", ContentBased, {"features_method": "title_length", "regressor_method": "linear_regression_false"}),
-        ("LinearRegression_Intercept_True", ContentBased, {"features_method": "title_length", "regressor_method": "linear_regression_true"}),
+        # ("LinearRegression_Intercept_False", ContentBased, {"features_method": "title_length", "regressor_method": "linear_regression_false"}),
+        # ("LinearRegression_Intercept_True", ContentBased, {"features_method": "title_length", "regressor_method": "linear_regression_true"}),
         
         ("ContentBased_ridge_cv", ContentBased, {"features_method": "all_content_tmdb_tags2000", "regressor_method": "ridge_cv"}), # best so far
-        ("ContentBased_ridge", ContentBased, {"features_method": "all_content_tmdb_tags2000", "regressor_method": "ridge"}) 
+        # ("ContentBased_ridge", ContentBased, {"features_method": "all_content_tmdb_tags2000", "regressor_method": "ridge"}),
 
     ]
 
     # Evaluation metrics. Only rmse is activated here for computational reasons, but you can activate more metrics if you want.
-    split_metrics = ["rmse"] # = "mae"
-    loo_metrics = [] # hit_rate
-    full_metrics = [] # novelty
+    split_metrics = ["rmse", "mae"]
+    loo_metrics = ["hit_rate", "ndcg@5", "ndcg@10", "ndcg@20"]
+    full_metrics = ["coverage", "miuf", "ild"]
 
     # Split parameters
     test_size = 0.25  # -- configure the test_size (from 0 to 1) --
 
     # Loo parameters
-    top_n_value = 40  # -- configure the numer of recommendations (> 1) --
+    top_n_value = 80  # -- configure the numer of recommendations (> 1) --
